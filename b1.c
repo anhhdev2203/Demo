0a77147d55b30ed5dc2654ff2d2eb738c4a9ae96
anhhdev2203 <134388379+anhhdev2203@users.noreply.github.com>
2023-05-23 19:24:17 +0700

    Revert "hh"
    This reverts commit 1f9f03b7e66a726b16b385a4f994e96da78ce7a5.

--- b1.c
+++ b1.c
@@ -5,6 +5,9 @@ int main ( ){
 	printf ("Nhap gia tri x = ");
 	scanf ("%d", &x);
 	if ( x > 0) {
-		printf ("Can bac 2 
+		printf ("Can bac 2 cua x = %.3f", sqrt(x));
+	} else {
+		printf ("Gia tri x = %d", x);
+	}
 	
 }
