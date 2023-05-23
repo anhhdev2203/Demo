1f9f03b7e66a726b16b385a4f994e96da78ce7a5
anhhdev2203 <134388379+anhhdev2203@users.noreply.github.com>
2023-05-23 19:23:47 +0700

    hh

--- b1.c
+++ b1.c
@@ -5,9 +5,6 @@ int main ( ){
 	printf ("Nhap gia tri x = ");
 	scanf ("%d", &x);
 	if ( x > 0) {
-		printf ("Can bac 2 cua x = %.3f", sqrt(x));
-	} else {
-		printf ("Gia tri x = %d", x);
-	}
+		printf ("Can bac 2 
 	
 }
